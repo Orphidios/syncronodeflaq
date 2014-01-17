3953a8038475232c0afabee865b815f2027e18a1
OpenShift guest <ripoche.sylvain.isart@gmail.com>
2014-01-17 09:50:04 -0500

    change app.js

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ var config = require('./config')(app, express, io);
 // Including a routing system
 require('./routes/routes')(app);
 
-// Listening port 3000
+// Listening on port 3000
 server.listen(process.env.PORT || 3000);
